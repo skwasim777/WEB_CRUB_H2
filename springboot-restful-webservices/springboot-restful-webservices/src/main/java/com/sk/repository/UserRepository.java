2751f9c861e39a100dcf02bd69f6240e89bd074e
Shobhith Lakkakula <shobhith.lakkakula@tcpwave.com>
2023-08-15 22:27:41 +0530

    first commit...

---
+++ springboot-restful-webservices/springboot-restful-webservices/src/main/java/com/sk/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.sk.repository;
+
+import com.sk.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ springboot-restful-webservices/springboot-restful-webservices/src/main/java/com/sk/service/impl/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package com.sk.service.impl;
+
+import com.sk.entity.User;
+import com.sk.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import com.sk.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User getUserById(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        return optionalUser.get();
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User updateUser(User user) {
+        User existingUser = userRepository.findById(user.getId()).get();
+        existingUser.setFirstName(user.getFirstName());
+        existingUser.setLastName(user.getLastName());
+        existingUser.setEmail(user.getEmail());
+        User updatedUser = userRepository.save(existingUser);
+        return updatedUser;
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+        userRepository.deleteById(userId);
+    }
+}
